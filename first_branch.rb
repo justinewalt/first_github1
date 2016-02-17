3c94b96d30c90d1187d45df0f183dc123645ae51
Justin Ewalt <justinewalt@gmail.com>
2016-02-17 11:33:20 -0700

    added first branch

---
+++ first_branch.rb
@@ -0,0 +1,8 @@
+puts "First Branch"
+puts "What is your favorite Color?"
+favorite_color == gets.strip.downcase
+if favorite_color == "black"
+	puts "you are depressed, bro?"
+else
+	puts "Nice."
+end
