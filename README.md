d53f7f973a290fccb80b781efec3f3db5b4f10ba
Justin Ewalt <justinewalt@gmail.com>
2016-02-17 11:18:25 -0700

    changed readme and github files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# first_github1
+# First Github1
+# Learning Github
+# Justin Ewalt
--- github.rb
+++ github.rb
@@ -1,3 +1,8 @@
 puts "Hello Github"
 puts "What is your name?"
 name = gets.strip
+if name == "Justin Ewalt"
+	puts "What's up developer"
+else
+	puts "Hello person"
+end
